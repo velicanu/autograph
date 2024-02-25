c58d69e5455981bbad17b1262306edafab34e5e8
Dragos Velicanu <dragosvelicanu@gmail.com>
2024-02-25 18:05:06 -0500

    add grapher

--- src/app.py
+++ src/app.py
@@ -1,10 +1,52 @@
+import os
+import tempfile
+
+import pandas as pd
+import plotly.express as px
 import streamlit as st
 
 
-def main():
-    st.write("Hello World!")
+def get_graphs(path):
+    return sorted([v.replace(".md", "") for v in os.listdir(path) if v.endswith(".md")])
+
+
+def df_from_md(filename):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        with open(os.path.join(tmpdir, "out"), "w") as fout, open(filename) as fin:
+            fout.write(fin.read().replace(" - ", ","))
+        return pd.read_csv(os.path.join(tmpdir, "out"), sep=",")
+
+
+def line(df, title):
+    fig = px.line(df, x=df.columns[0], y=df.columns[1], title=title)
+    st.plotly_chart(fig, use_container_width=True)
+
+
+def plot(datadir, graph):
+    df = df_from_md(os.path.join(datadir, graph + ".md"))
+    if graph.endswith("-line"):
+        line(df, graph.replace("-line", ""))
+
+
+def main(datadir):
+    default_graphs = st.query_params.get_all("graphs")
+    graphs_ = get_graphs(datadir)
+    print(default_graphs)
+
+    graphs = st.multiselect(
+        label="Graphs",
+        options=graphs_,
+        default=default_graphs
+        if "graphs" not in st.session_state
+        else st.session_state["graphs"],
+        key="graphs",
+    )
+    st.query_params["graphs"] = graphs
+    for graph in graphs:
+        plot(datadir=datadir, graph=graph)
+
     return "done"
 
 
 if __name__ == "__main__":
-    main()
+    main("data")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,27 @@
-from app import main
+import os
+
+from app import df_from_md, get_graphs, main
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+examples_dir = os.path.join(script_dir, "../examples")
 
 
 def test_main():
     expected = "done"
-    actual = main()
+    actual = main(examples_dir)
+    assert actual == expected
+
+
+def test_df_from_md():
+    expected = {
+        "date": {0: "2023-11-23", 1: "2023-12-14", 2: "2023-12-26", 3: "2024-01-23"},
+        "height": {0: 52.5, 1: 53.3, 2: 55.2, 3: 59.7},
+    }
+    actual = df_from_md(os.path.join(examples_dir, "example-line.md")).to_dict()
+    assert actual == expected
+
+
+def test_get_graphs():
+    expected = ["example-line"]
+    actual = get_graphs(examples_dir)
     assert actual == expected
